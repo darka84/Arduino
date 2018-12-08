fb485ac922d752793f32a42d2ad270742ceaf02d
darka84 <darkaster@gmail.com>
2018-12-08 17:47:32 +0200

    Add files via upload

---
+++ Animacija_Kaledoms/Animacija_Kaledoms.ino
@@ -0,0 +1,417 @@
+// Adafruit_NeoMatrix example for single NeoPixel Shield.
+// By Marc MERLIN <marc_soft@merlins.org>
+// Contains code (c) Adafruit, license BSD
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Fonts/FreeSans9pt7b.h>
+// Choose your prefered pixmap
+//#include "heart24.h"
+//#include "yellowsmiley24.h"
+//#include "bluesmiley24.h"
+//#include "smileytongue24.h"
+//#include "santa.h"
+//#include "saldainis.h"
+
+#define PIN 6
+
+
+
+// Max is 255, 32 is a conservative value to not overload
+// a USB power supply (500mA) for 12x12 pixels.
+#define BRIGHTNESS 255
+
+// Define matrix width and height.
+#define mw 20
+#define mh 30
+
+// MATRIX DECLARATION:
+// Parameter 1 = width of EACH NEOPIXEL MATRIX (not total display)
+// Parameter 2 = height of each matrix
+// Parameter 3 = number of matrices arranged horizontally
+// Parameter 4 = number of matrices arranged vertically
+// Parameter 5 = pin number (most are valid)
+// Parameter 6 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the FIRST MATRIX; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs WITHIN EACH MATRIX are
+//     arranged in horizontal rows or in vertical columns, respectively;
+//     pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns WITHIN
+//     EACH MATRIX proceed in the same order, or alternate lines reverse
+//     direction; pick one.
+//   NEO_TILE_TOP, NEO_TILE_BOTTOM, NEO_TILE_LEFT, NEO_TILE_RIGHT:
+//     Position of the FIRST MATRIX (tile) in the OVERALL DISPLAY; pick
+//     two, e.g. NEO_TILE_TOP + NEO_TILE_LEFT for the top-left corner.
+//   NEO_TILE_ROWS, NEO_TILE_COLUMNS: the matrices in the OVERALL DISPLAY
+//     are arranged in horizontal rows or in vertical columns, respectively;
+//     pick one or the other.
+//   NEO_TILE_PROGRESSIVE, NEO_TILE_ZIGZAG: the ROWS/COLUMS OF MATRICES
+//     (tiles) in the OVERALL DISPLAY proceed in the same order for every
+//     line, or alternate lines reverse direction; pick one.  When using
+//     zig-zag order, the orientation of the matrices in alternate rows
+//     will be rotated 180 degrees (this is normal -- simplifies wiring).
+//   See example below for these values in action.
+// Parameter 7 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 pixels)
+//   NEO_GRB     Pixels are wired for GRB bitstream (v2 pixels)
+//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA v1 pixels)
+//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip)
+
+Adafruit_NeoMatrix *matrix = new Adafruit_NeoMatrix(mw, mh, PIN,
+  NEO_MATRIX_BOTTOM     + NEO_MATRIX_RIGHT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB            + NEO_KHZ800);
+
+// This could also be defined as matrix->color(255,0,0) but those defines
+// are meant to work for adafruit_gfx backends that are lacking color()
+#define LED_BLACK    0
+
+#define LED_RED_VERYLOW   (3 <<  11)
+#define LED_RED_LOW     (7 <<  11)
+#define LED_RED_MEDIUM    (15 << 11)
+#define LED_RED_HIGH    (31 << 11)
+
+#define LED_GREEN_VERYLOW (1 <<  5)   
+#define LED_GREEN_LOW     (15 << 5)  
+#define LED_GREEN_MEDIUM  (31 << 5)  
+#define LED_GREEN_HIGH    (63 << 5)  
+
+#define LED_BLUE_VERYLOW  3
+#define LED_BLUE_LOW    7
+#define LED_BLUE_MEDIUM   15
+#define LED_BLUE_HIGH     31
+
+#define LED_ORANGE_VERYLOW  (LED_RED_VERYLOW + LED_GREEN_VERYLOW)
+#define LED_ORANGE_LOW    (LED_RED_LOW     + LED_GREEN_LOW)
+#define LED_ORANGE_MEDIUM (LED_RED_MEDIUM  + LED_GREEN_MEDIUM)
+#define LED_ORANGE_HIGH   (LED_RED_HIGH    + LED_GREEN_HIGH)
+
+#define LED_PURPLE_VERYLOW  (LED_RED_VERYLOW + LED_BLUE_VERYLOW)
+#define LED_PURPLE_LOW    (LED_RED_LOW     + LED_BLUE_LOW)
+#define LED_PURPLE_MEDIUM (LED_RED_MEDIUM  + LED_BLUE_MEDIUM)
+#define LED_PURPLE_HIGH   (LED_RED_HIGH    + LED_BLUE_HIGH)
+
+#define LED_CYAN_VERYLOW  (LED_GREEN_VERYLOW + LED_BLUE_VERYLOW)
+#define LED_CYAN_LOW    (LED_GREEN_LOW     + LED_BLUE_LOW)
+#define LED_CYAN_MEDIUM   (LED_GREEN_MEDIUM  + LED_BLUE_MEDIUM)
+#define LED_CYAN_HIGH   (LED_GREEN_HIGH    + LED_BLUE_HIGH)
+
+#define LED_WHITE_VERYLOW (LED_RED_VERYLOW + LED_GREEN_VERYLOW + LED_BLUE_VERYLOW)
+#define LED_WHITE_LOW   (LED_RED_LOW     + LED_GREEN_LOW     + LED_BLUE_LOW)
+#define LED_WHITE_MEDIUM  (LED_RED_MEDIUM  + LED_GREEN_MEDIUM  + LED_BLUE_MEDIUM)
+#define LED_WHITE_HIGH    (LED_RED_HIGH    + LED_GREEN_HIGH    + LED_BLUE_HIGH)
+
+#define LED_YELLOW_HIGH    (LED_RED_HIGH    + LED_GREEN_HIGH  )
+
+#define SD_CS 53
+
+File root;
+int num_frames=0;
+
+void setup() {
+    Serial.begin(115200);
+    matrix->begin();
+    matrix->setTextWrap(false);
+    matrix->setBrightness(BRIGHTNESS);
+    // Test full bright of all LEDs. If brightness is too high
+    // for your current limit (i.e. USB), decrease it.
+    matrix->fillScreen(LED_WHITE_HIGH);
+    matrix->show();
+    delay(1000);
+    matrix->clear();
+    matrix->show();
+
+
+    //Serial.begin(9600);
+
+  pinMode(SD_CS, OUTPUT);
+  digitalWrite(SD_CS, HIGH);
+
+
+  
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("failed!");
+    return;
+  }
+  Serial.println("OK!");
+
+ root = SD.open("/");
+RunFiles(root);
+//display_scrollText() ;
+  //;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+#define BUFFPIXEL 20
+void bmpDraw(char *filename, int16_t x, int16_t y) {
+
+  File     bmpFile;
+  int      bmpWidth, bmpHeight;   // W+H in pixels
+  uint8_t  bmpDepth;              // Bit depth (currently must be 24)
+  uint32_t bmpImageoffset;        // Start of image data in file
+  uint32_t rowSize;               // Not always = bmpWidth; may have padding
+  uint8_t  sdbuffer[3*BUFFPIXEL]; // pixel buffer (R+G+B per pixel)
+  uint8_t  buffidx = sizeof(sdbuffer); // Current position in sdbuffer
+  boolean  goodBmp = false;       // Set to true on valid header parse
+  boolean  flip    = true;        // BMP is stored bottom-to-top
+  int      w, h, row, col;
+  uint8_t  r, g, b;
+  uint32_t pos = 0, startTime = millis();
+  int xx=0;
+  int yy=0;
+  if((x >= mw) || (y >= mh)) return;
+
+  Serial.println();
+  Serial.print("Loading image '");
+  Serial.print(filename);
+  Serial.println('\'');
+
+  // Open requested file on SD card
+  if ((bmpFile = SD.open(filename)) == NULL) {
+    Serial.print("File not found");
+    return;
+  }
+
+  // Parse BMP header
+  if(read16(bmpFile) == 0x4D42) { // BMP signature
+    Serial.print("File size: "); Serial.println(read32(bmpFile));
+    (void)read32(bmpFile); // Read & ignore creator bytes
+    bmpImageoffset = read32(bmpFile); // Start of image data
+    Serial.print("Image Offset: "); Serial.println(bmpImageoffset, DEC);
+    // Read DIB header
+    Serial.print("Header size: "); Serial.println(read32(bmpFile));
+    bmpWidth  = read32(bmpFile);
+    bmpHeight = read32(bmpFile);
+    if(read16(bmpFile) == 1) { // # planes -- must be '1'
+      bmpDepth = read16(bmpFile); // bits per pixel
+      Serial.print("Bit Depth: "); Serial.println(bmpDepth);
+      if((bmpDepth == 24) && (read32(bmpFile) == 0)) { // 0 = uncompressed
+
+        goodBmp = true; // Supported BMP format -- proceed!
+        Serial.print("Image size: ");
+        Serial.print(bmpWidth);
+        Serial.print('x');
+        Serial.println(bmpHeight);
+
+        // BMP rows are padded (if needed) to 4-byte boundary
+        rowSize = (bmpWidth * 3 + 3) & ~3;
+
+        // If bmpHeight is negative, image is in top-down order.
+        // This is not canon but has been observed in the wild.
+        if(bmpHeight < 0) {
+          bmpHeight = -bmpHeight;
+          flip      = false;
+        }
+
+        // Crop area to be loaded
+        w = bmpWidth;
+        h = bmpHeight;
+
+        if((x+w-1) >= mw)  w = mw  - x;
+        if((y+h-1) >= mh) h = mh - y;
+
+        // Set TFT address window to clipped image bounds
+       // tft.setAddrWindow(x, y, x+w-1, y+h-1);
+
+        for (row=0; row<h; row++) { // For each scanline...
+
+          // Seek to start of scan line.  It might seem labor-
+          // intensive to be doing this on every line, but this
+          // method covers a lot of gritty details like cropping
+          // and scanline padding.  Also, the seek only takes
+          // place if the file position actually needs to change
+          // (avoids a lot of cluster math in SD library).
+          if(flip) // Bitmap is stored bottom-to-top order (normal BMP)
+            pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
+          else     // Bitmap is stored top-to-bottom
+            pos = bmpImageoffset + row * rowSize;
+          if(bmpFile.position() != pos) { // Need seek?
+            bmpFile.seek(pos);
+            buffidx = sizeof(sdbuffer); // Force buffer reload
+          }
+
+          for (col=0; col<w; col++) { // For each pixel...
+            // Time to read more pixel data?
+            if (buffidx >= sizeof(sdbuffer)) { // Indeed
+              bmpFile.read(sdbuffer, sizeof(sdbuffer));
+              buffidx = 0; // Set index to beginning
+            }
+
+            // Convert pixel from BMP to TFT format, push to display
+            b = sdbuffer[buffidx++];
+            g = sdbuffer[buffidx++];
+            r = sdbuffer[buffidx++];
+           // tft.pushColor(tft.Color565(r,g,b));
+           //fixdrawRGBBitmap
+           //fixdrawRGBBitmap(x, y, RGB_bmp[10], 8, 8);
+           xx=col+x;
+           yy=row+y;
+           matrix->drawPixel( xx,yy,  (const uint16_t) ConvertRGB(r,g,b));
+         //matrix->rawRGBBitmap(x, y, (const uint16_t *) sdbuffer, w, h);
+          } // end pixel
+        } // end scanline
+        
+        Serial.print("Loaded in ");
+        Serial.print(millis() - startTime);
+        Serial.println(" ms");
+        // matrix->show(); 
+      } // end goodBmp
+    }
+  }
+
+  bmpFile.close();
+  if(!goodBmp) Serial.println("BMP format not recognized.");
+}
+
+// These read 16- and 32-bit types from the SD card file.
+// BMP data is stored little-endian, Arduino is little-endian too.
+// May need to reverse subscript order if porting elsewhere.
+
+uint16_t read16(File & f) {
+  uint16_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read(); // MSB
+  return result;
+}
+
+uint32_t read32(File & f) {
+  uint32_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read();
+  ((uint8_t *)&result)[2] = f.read();
+  ((uint8_t *)&result)[3] = f.read(); // MSB
+  return result;
+}
+
+word ConvertRGB( byte R, byte G, byte B)
+{
+  return ( ((R & 0xF8) << 8) | ((G & 0xFC) << 3) | (B >> 3) );
+}
+
+void RunFiles(File dir ) {
+  while (true) {
+
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      dir.rewindDirectory();
+      //break;
+    }
+
+    if (!entry.isDirectory()) {
+      if (String(entry.name()).endsWith("BMP") && entry.name()!="FILLER.BMP" )
+      {
+        num_frames++;
+        //bmpDraw(entry.name(), 0, 0);
+        //display_scrollText();
+        display_panOrBounceBitmap(entry.name(),32,32);
+        display_panOrBounceBitmap("FILLER.BMP",32,32);
+        //delay(5000);
+      }
+    } 
+    entry.close();
+  }
+}
+
+
+void display_panOrBounceBitmap (char *filename, uint8_t bitmapSizeh,uint8_t bitmapSizew) {
+    // keep integer math, deal with values 16 times too big
+    // start by showing upper left of big bitmap or centering if the display is big
+    int16_t xf = max(0, (mw-bitmapSizeh)/2) << 4;
+    int16_t yf = max(0, (mh-bitmapSizeh)/2) << 4;
+    // scroll speed in 1/16th
+    int16_t xfc = 6;
+    int16_t yfc = 3;
+    // scroll down and right by moving upper left corner off screen 
+    // more up and left (which means negative numbers)
+    int16_t xfdir = -1;
+    int16_t yfdir = -1;
+
+    for (uint16_t i=1; i<100; i++) {
+  bool updDir = false;
+
+  // Get actual x/y by dividing by 16.
+  int16_t x = xf >> 4;
+  int16_t y = yf >> 4;
+
+  matrix->clear();
+
+ bmpDraw(filename, x, y);
+ Serial.print(x);
+ Serial.print(",");
+  Serial.println(y);
+  matrix->show();
+
+   
+  // Only pan if the display size is smaller than the pixmap
+  // but not if the difference is too small or it'll look bad.
+  if (bitmapSizew-mw>2) {
+      if (mw>9) xf += xfc*xfdir;
+      if (xf >= 0)                      { xfdir = -1; updDir = true ; };
+      // we don't go negative past right corner, go back positive
+      if (xf <= ((mw-bitmapSizew) << 4)) { xfdir = 1;  updDir = true ; };
+  }
+  if (bitmapSizeh-mh>2) {
+      yf += yfc*yfdir;
+      // we shouldn't display past left corner, reverse direction.
+      if (yf >= 0)                      { yfdir = -1; updDir = true ; };
+      if (yf <= ((mh-bitmapSizeh) << 4)) { yfdir = 1;  updDir = true ; };
+  }
+  // only bounce a pixmap if it's smaller than the display size
+  if (mw>bitmapSizew) {
+      xf += xfc*xfdir;
+      // Deal with bouncing off the 'walls'
+      if (xf >= (mw-bitmapSizew) << 4) { xfdir = -1; updDir = true ; };
+      if (xf <= 0)           { xfdir =  1; updDir = true ; };
+  }
+  if (mh>bitmapSizeh) {
+      yf += yfc*yfdir;
+      if (yf >= (mh-bitmapSizeh) << 4) { yfdir = -1; updDir = true ; };
+      if (yf <= 0)           { yfdir =  1; updDir = true ; };
+  }
+  
+  if (updDir) {
+      // Add -1, 0 or 1 but bind result to 1 to 1.
+      // Let's take 3 is a minimum speed, otherwise it's too slow.
+      xfc = constrain(xfc + random(-1, 2), 3, 16);
+      yfc = constrain(xfc + random(-1, 2), 3, 16);
+  }
+
+  delay(10);
+    }
+}
+
+
+void display_scrollText() {
+  //matrix->setFont(&FreeSans9pt7b);
+    matrix->clear();
+    matrix->setTextWrap(false);  // we don't wrap text so it scrolls nicely
+    matrix->setTextSize(1);
+    matrix->setRotation(0);
+    int i=0;
+    for (int8_t x=7; x>=-42; x--) {
+  matrix->clear();
+  matrix->fillScreen(LED_PURPLE_LOW);
+  
+  matrix->setCursor(5,5);
+
+  matrix->setTextColor(LED_RED_HIGH);
+
+    if (i==3){ i=0; } else {matrix->print("SU"); i++;}
+  matrix->setCursor(x,15);
+  matrix->setTextColor(LED_BLUE_HIGH);
+  matrix->print("KALEDOM");
+  matrix->show();
+       delay(150);
+    }
+}
